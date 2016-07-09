2707506c36757ece7b6da79edb0396041a16a0b9
ysytnik <sytniky@gmail.com>
2016-07-10 00:24:31 +0300

    test

--- app/src/main/java/edu/hillel/activityresult/MainActivity.java
+++ app/src/main/java/edu/hillel/activityresult/MainActivity.java
@@ -1,14 +1,17 @@
 package edu.hillel.activityresult;
 
 import android.content.Intent;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    public static final String LOG_TAG = MainActivity.class.getSimpleName();
     public static final int SECOND_ACTIVITY_CODE = 1001;
     private TextView tvText;
 
@@ -20,6 +23,43 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button btn  = (Button) findViewById(R.id.btnGetValue);
         btn.setOnClickListener(this);
         tvText = (TextView) findViewById(R.id.tvText);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        Log.d(LOG_TAG, "onCreate()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "onStart()");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy()");
     }
 
     @Override
@@ -36,7 +76,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 tvText.setText(data.getStringExtra("value"));
             }
         } else if (resultCode == RESULT_CANCELED) {
-            tvText.setText("CANCELED");
+            tvText.setText(data.getStringExtra("value"));
         }
     }
 }
--- app/src/main/java/edu/hillel/activityresult/SecondActivity.java
+++ app/src/main/java/edu/hillel/activityresult/SecondActivity.java
@@ -3,13 +3,14 @@ package edu.hillel.activityresult;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity implements View.OnClickListener {
 
+    public static final String LOG_TAG = SecondActivity.class.getSimpleName();
     private EditText et;
 
     @Override
@@ -19,6 +20,38 @@ public class SecondActivity extends AppCompatActivity implements View.OnClickLis
         et = (EditText) findViewById(R.id.edText);
         Button btn = (Button) findViewById(R.id.btnSetValue);
         btn.setOnClickListener(this);
+        Log.d(LOG_TAG, "onCreate()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "onStart()");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy()");
     }
 
     @Override
